89c5a996da46e983c9d382eada69159c3d82ef93
StefKal <35640421+StefKal@users.noreply.github.com>
2018-12-09 19:03:19 -0500

    restructured board, now background is drawn

--- app/src/main/java/edu/stlawu/hockeyair/Board.java
+++ app/src/main/java/edu/stlawu/hockeyair/Board.java
@@ -5,90 +5,76 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.Point;
+import android.graphics.Rect;
 import android.graphics.RectF;
 import android.graphics.Region;
 
 public class Board implements GameObject {
 
-    static int MYGOAL = 1;
-    static int OPPONENTGOAL = 2;
-
-    // static vars for collisionPos
-    static int LEFT_TOP = 10;
-    static int RIGHT_TOP = 11;
-    static int RIGHT_BOTTOM = 12;
-    static int RIGHT = 13;
-    static int LEFT = 14;
-    static int BOTTOM = 15;
-    static int TOP = 16;
-
-    private Path myBoard;
+    private Path playerBoard;
     private Path opponentBoard;
     private Path fullBoard;
+    private Path playerGoal;
+    private Path opponentGoal;
+    //test
 
-    private RectF myGoal;
-    private RectF opponentGoal;
+    private int boardWidth;
+    private int boardHeight;
+
+
+    // topLeft
+    private int topLeftX = 0;
+    private int topLeftY = 0;
+
+    // topRight
+    private int topRightX = ScreenConstants.SCREEN_WIDTH;
+    private int topRightY = 0;
+
+    // botLeft
+    private int botLeftX = 0;
+    private int botLeftY = ScreenConstants.SCREEN_HEIGHT;
+
+    // botRight
+    private int botRightX = ScreenConstants.SCREEN_WIDTH;
+    private int botRightY = ScreenConstants.SCREEN_HEIGHT;
+
+    // centerPoints
+    private int yCenter = ScreenConstants.SCREEN_HEIGHT/2;
 
     private int boardColor;
 
-    private float xTopRight;
-    private float xTopLeft;
-
-    private float xInterceptLeft;
-    private float xInterceptRight;
-
-    private float xCenter;
-    private float yCenter;
-
     public Board(int boardColor){
-
         this.boardColor = boardColor;
-        xTopLeft = ScreenConstants.SCREEN_WIDTH;
-        xTopRight = ScreenConstants. SCREEN_WIDTH;
-        getCenter();
 
-        fullBoard = drawRectangle(0,ScreenConstants.SCREEN_HEIGHT,
-                ScreenConstants.SCREEN_WIDTH,
-                ScreenConstants.SCREEN_HEIGHT,
-                xTopRight,0,xTopLeft,0);
+        this.fullBoard = makeRectanglePath(topLeftX, topLeftY, topRightX, topRightY, botLeftX, botLeftY, botRightX, botRightY);
 
-        myBoard = drawRectangle(0,ScreenConstants.SCREEN_HEIGHT,ScreenConstants.SCREEN_WIDTH,ScreenConstants.SCREEN_HEIGHT,
-                xInterceptRight,yCenter,xInterceptLeft,yCenter);
+        this.playerBoard = makeRectanglePath(topLeftX, yCenter, topRightX, yCenter, botLeftX, botLeftY, botRightX, botRightY);
+        this.opponentBoard = makeRectanglePath(topLeftX, topLeftY, topRightX, topRightY, botLeftX, yCenter, botRightX, yCenter);
 
-        opponentBoard = drawRectangle(xInterceptLeft,yCenter,xInterceptRight,yCenter,
-                xTopRight,0,xTopLeft,0);
+        //this.playerGoal = makeRectanglePath();
+        //this.opponentGoal = makeRectanglePath();
 
-        myGoal = new RectF((ScreenConstants.SCREEN_WIDTH/2)-
-                (ScreenConstants.SCREEN_WIDTH/4),
-                ScreenConstants.SCREEN_HEIGHT-20,
-                (ScreenConstants.SCREEN_WIDTH/2)+(ScreenConstants.SCREEN_WIDTH/4),
-                ScreenConstants.SCREEN_HEIGHT+20);
-
-        // define constant multiplier
-        double multiplier = ScreenConstants.getMultiplier(new Point(0,0));
-
-        opponentGoal = new RectF((ScreenConstants.SCREEN_WIDTH/2)-((int)(multiplier*ScreenConstants.SCREEN_WIDTH/4)),
-                -20, (ScreenConstants.SCREEN_WIDTH/2)+((int)(multiplier*ScreenConstants.SCREEN_WIDTH/4)),
-                +20);
+        this.boardWidth = ScreenConstants.SCREEN_WIDTH;
+        this.boardHeight = ScreenConstants.SCREEN_HEIGHT;
 
 
-//        decoration=new RectF(xCenter-ScreenConstants.SCREEN_WIDTH/8,
-//                yCenter-ScreenConstants.SCREEN_HEIGHT/9,
-//                xCenter +ScreenConstants.SCREEN_WIDTH/6,yCenter+ScreenConstants.SCREEN_HEIGHT/10);
-//
+
     }
 
+    private float getDistance(float x1, float y1, float x2, float y2){
+        return (float) Math.sqrt(((x2 - x1) * (x2 -x1)) + ((y2 - y1) * (y2 - y1)));
+    }
 
-    //draws a the board
-    public Path drawRectangle(float x1,float y1,float x2,float y2,float x3,float y3, float x4, float y4){
+    //draws the board
+    private Path makeRectanglePath(float x1, float y1, float x2, float y2, float x3, float y3, float x4, float y4){
         Path rectangle = new Path();
-        rectangle.reset();
 
-        rectangle.moveTo(x1,y1);
-        rectangle.lineTo(x2,y2);
-        rectangle.lineTo(x3,y3);
-        rectangle.lineTo(x4,y4);
-        rectangle.lineTo(x1,y1);
+        float left = getDistance(x1, y1,x3,y3);
+        float top = getDistance(x1,y1, x2, y2);
+        float right = getDistance(x2, y2, x4, y4);
+        float bot = getDistance(x3, y3, x4, y4);
+
+        rectangle.addRect(left, top, right, bot, Path.Direction.CW);
 
         return rectangle;
     }
@@ -98,63 +84,30 @@ public class Board implements GameObject {
     public void draw(Canvas canvas) {
         Paint paint = new Paint();
         paint.setColor(boardColor);
-        canvas.drawPath(myBoard, paint);
-        canvas.drawPath(opponentBoard, paint);
+        canvas.drawPath(playerGoal,paint);
+        canvas.drawPath(opponentBoard,paint);
         paint.setColor(Color.BLACK);
         paint.setStrokeWidth(15);
-        canvas.drawLine(xInterceptLeft, yCenter, xInterceptRight,yCenter, paint);
+        canvas.drawText("FUCK THAT",ScreenConstants.SCREEN_WIDTH/2, yCenter, paint);
+
         paint.setStyle(Paint.Style.STROKE);
         paint.setColor(Color.BLACK);
-        //canvas.drawOval(decoration, paint);
+
         paint.setColor(Color.BLACK);
-        canvas.drawRect(myGoal, paint);
-        canvas.drawRect(opponentGoal, paint);
 
 
     }
-
 
     @Override
     public void update(Point point) {
 
     }
 
-    // gets center of screen by returning yCenter
-    public double getCenterY(){
-        return yCenter;
-    }
-
-    //gets center view
-    private void getCenter(){
-        double slope_one = ScreenConstants.SCREEN_HEIGHT / -xTopRight;
-        double slope_two = ScreenConstants.SCREEN_HEIGHT / (ScreenConstants.SCREEN_WIDTH-xTopLeft);
-
-        double B1 = (double)ScreenConstants.SCREEN_HEIGHT;
-        double B2 = (double)ScreenConstants.SCREEN_HEIGHT - (slope_two * ScreenConstants.SCREEN_WIDTH);
-
-        xCenter = (float)((B2-B1) / (slope_one - slope_two));
-        yCenter = (float)((xCenter * slope_one) + ScreenConstants.SCREEN_HEIGHT);
-
-        System.out.println("xCenter: " + xCenter);
-        System.out.println("XLength: " + ScreenConstants.SCREEN_WIDTH);
-
-        // get intercepts
-        getIntercepts();
-    }
-
-    // get intercepts
-    private void getIntercepts(){
-        double length_one = xTopRight - xTopLeft;
-        double length_two = ScreenConstants.SCREEN_WIDTH;
-
-        double midLength =(2 * length_one * length_two) / (length_one + length_two);
-
-        xInterceptLeft =(float)(xCenter - midLength/2);
-        xInterceptRight =(float)(xCenter + midLength/2);
+/*
+    @Override
+    public void update(Point point) {
 
     }
-
-    // check if goal was scored and which goal it was scored on
     public int scoredGoal(Puck puck) {
 
         // board screen
@@ -166,7 +119,7 @@ public class Board implements GameObject {
         Path hockeyPuck = new Path();
 
         // draw rect for goal and puck to check intersect
-        goal.addRect(myGoal,Path.Direction.CCW); // CCW Counter Clockwise
+        goal.addRect(playerGoal,Path.Direction.CCW); // CCW Counter Clockwise
         oppGoal.addRect(opponentGoal, Path.Direction.CCW);
         hockeyPuck.addOval(puck.getPuck(), Path.Direction.CCW);
 
@@ -266,5 +219,5 @@ public class Board implements GameObject {
 
 
     }
-
+*/
 }
